6031253d7343b5a685e8c022e5b9f06fcb011377
Evan Knebel <s179060@students.aie.edu.au>
2018-04-04 11:44:58 -0700

    first commit

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public float spawnInterval;
+    float timer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void spawnThing()
+    {
+        GameObject spawnedThing = Instantiate(myPrefab);
+        spawnedThing.transform.position = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            spawnThing();
+            timer = spawnInterval;
+        }
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject target;
+    public GameObject myPrefab;
+    public float speed;
+    public float shootInterval;
+    float timer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void spawnThing()
+    {
+        GameObject spawnedThing = Instantiate(myPrefab);
+        spawnedThing.transform.position = transform.position;
+        Vector3 shootDir = (target.transform.position - spawnedThing.transform.position).normalized;
+        spawnedThing.GetComponent<Rigidbody>().velocity = shootDir.normalized * speed;
+        spawnedThing.transform.forward = shootDir.normalized;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            spawnThing();
+            timer = shootInterval;
+        }
+    }
+}
